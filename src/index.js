fef3c211d29b88f2ab12d0a116c8e4c92aef6fba
droneal11015 <droneal11015@gmail.com>
2019-05-02 16:21:40 -0400

    completed array

--- src/index.js
+++ src/index.js
@@ -3,3 +3,47 @@ import { render } from 'react-dom';
 import App from './App';
 
 render(<App />, document.getElementById('root'));
+
+
+const todolist = [
+    {
+      task: "Clean Bathroom",
+      id: 1,
+      completed: false
+    },
+    {
+        task: "Wash Dishes",
+        id: 2,
+        completed: false
+      },
+      {
+        task: "Organize Pantry",
+        id: 3,
+        completed: false
+      },
+      {
+        task: "Wash Dogs",
+        id: 4,
+        completed: false
+      },
+      {
+        task: "Put Away Winter Clothes",
+        id: 5,
+        completed: false
+      },
+      {
+        task: "Clean Kids Room",
+        id: 6,
+        completed: false
+      },
+      {
+        task: "Feed Bearded Dragon",
+        id: 7,
+        completed: false
+      },
+      {
+        task: "Cut Grass",
+        id: 8,
+        completed: false
+      },
+  ];
